e0c4e7bf787af580a031e1cf40dda4dafc2b2ef8
NichtJens <nichtjens@gmail.com>
2023-08-12 14:47:49 +0200

    added Hex class

---
+++ hex.py
@@ -0,0 +1,16 @@
+
+class Hex(int):
+
+    def __repr__(self):
+        return hex(self)
+
+
+
+#TODO: make this part of the constructor?
+
+#    Convert literal string integers to integers. Note from int():
+#    For base 0, the string is interpreted in a similar way to an integer literal in code,
+#    in that the actual base is 2, 8, 10, or 16 as determined by the prefix. (b=2, o=8, x=16)
+
+
+
